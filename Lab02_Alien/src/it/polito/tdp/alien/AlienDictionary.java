7938e62be3b5e9241fe7865084cc1a72ba2c66b8
Cristiano <Cristiano@172.22.14.232>
2019-03-13 13:52:44 +0100

    Versione con classi

---
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienDictionary.java
@@ -0,0 +1,18 @@
+package it.polito.tdp.alien;
+
+import java.util.*;
+
+public class AlienDictionary {
+	private Map<String, WordEnhanced> dizionario;
+
+	
+	public AlienDictionary() {
+		dizionario = new HashMap<String, WordEnhanced>();
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ Lab02_Alien/src/it/polito/tdp/alien/WordEnhanced.java
@@ -0,0 +1,23 @@
+package it.polito.tdp.alien;
+
+import java.util.*;
+
+public class WordEnhanced {
+	private String alienWord;
+	private List<String> translations;
+
+	public WordEnhanced(String alienWord) {
+		this.alienWord = alienWord;
+		translations = new LinkedList<String>();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
